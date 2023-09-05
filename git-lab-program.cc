7e882097b2b6a56a908f3693482ebec351646a5c
Mason Peffers <mp118723@ohio.edu>
2023-09-05 17:15:58 -0400

    lab 2 code

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Mason Peffers
+ *        Date: September 5th, 2023
+ * Description: Lab 2 code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
